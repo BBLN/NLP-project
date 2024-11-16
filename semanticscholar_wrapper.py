e6fb0894bef99118b69731bdf5c5764e4e0872fd
Omer <BBLN@users.noreply.github.com>
2024-11-16 17:32:38 +0200

    bulk_search remove default limit

--- semanticscholar_wrapper.py
+++ semanticscholar_wrapper.py
@@ -5,7 +5,7 @@ class SemanticScholarWrapper:
         self.client = semanticscholar.SemanticScholar()
 
     def bulk_search(self, **kwargs):
-        return self.client.search_paper(**kwargs, limit=100, bulk=True)
+        return self.client.search_paper(**kwargs, bulk=True)
     
     def get_specter_embeddings(self, paper_ids):
         papers = self.client.get_papers(paper_ids, fields=["embedding.specter_v2"])
